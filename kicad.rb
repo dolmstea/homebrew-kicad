d53205e0cd3447cea3d2fe39d3bd486e612a54ed
David Olmstead <david.lorne.olmstead@gmail.com>
2014-10-28 17:16:37 -0400

    A great leap forward and a small stumble back.
    We have made it further. Added some patches to fix the boost building
    issues and restructured the way the compiler flags were being passed.
    It now uses an array that is passed to cmake rather than manually
    altering the environment variables.

--- kicad.rb
+++ kicad.rb
@@ -15,23 +15,30 @@ class Kicad < Formula
   depends_on "zlib"
   depends_on "freeglut"
   depends_on "glew"
-  depends_on "doxygen" => :optional #For doxygen documentation support. See kicad build docs.
+  depends_on "doxygen" => :recommended #For doxygen documentation support. See kicad build docs.
+  depends_on "wxpython" => :optional #Will need to develop this more.
+  
+  # This is a patch to fix the internal boost building stupidity. It forces the use of the
+  # c++11 libraries when compiling boost and sets the minimum OSX version to 10.7 so that
+  # goddamned clang and llvm don't have a fit.
+  patch :DATA
   
   # In a crazy world, could implement wxPython support option that adds a bunch of cmake args. See CMakeCache.txt
   
   def install
-    #ohai "We have to not use the standard cmake arguments because they don't work."
     
-    #Set some environment variables so that we use the correct C libraries when dealing with wxWidgets.
+    #Set some environment variables so that we use the correct C libraries when dealing with wxWidgets. I
+    #just deprecated this by using the args array to pass these as arguments to cmake rather than setting
+    #environment variables.
     ENV["CC"] = "clang"
-    ENV["CXX"] = "clang++"
-    ENV["CXXFLAGS"] = "-stdlib=libc++ -std=c++11" # -Wno-c++11-narrowing
-    ENV["OBJCXXFLAGS"] = "-stdlib=libc++ -std=c++11" # -Wno-c++11-narrowing
+    # ENV["CXX"] = "clang++"
+    # ENV["CXXFLAGS"] = "-stdlib=libc++ -std=c++11 -Wno-c++11-narrowing"
+    ENV["OBJCXXFLAGS"] = "-stdlib=libc++ -std=c++11 -Wno-c++11-narrowing"
     ENV["LDFLAGS"] = "-stdlib=libc++ -Wno-c++11-narrowing"
     
-    # Newest problem: The system's own boost is not being compiled with libc++11.
+    # Newest problem: The system's own boost may not be being compiled with libc++11. Fixed by patch?
     
-# Standard CMAKE args.
+# Standard CMAKE args for reference. Remove before release.
 #      "-DCMAKE_FIND_FRAMEWORK=LAST",
 #      "-DCMAKE_INSTALL_PREFIX=/usr/local/Cellar/kicad/HEAD",
 #      "-DCMAKE_BUILD_TYPE=NONE",
@@ -41,14 +48,24 @@ class Kicad < Formula
 
     # The OSX_DEPLOYMENT_TARGET flag is necessary, as clang needs the c++11 library specified,
     # but won't work unless we also tell it to only build for OSX versions after 10.7.
-    ohai *std_cmake_args
-    system "cmake", ".", *std_cmake_args, "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7"
+    
+    args = *std_cmake_args
+    args << "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7"
+    args << "-DCMAKE_CXX_COMPILER=clang++"
+    args << "-DCMAKE_CXX_FLAGS='-stdlib=libc++ -std=c++11 -Wno-c++11-narrowing'"
+    
+    ohai *args # Temporary.
+    ohai *std_cmake_args # Temporary.
+    
+    system "cmake", ".", *args # Removed: *std_cmake_args, "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7"
 
     system "make", "install"
   end
   
   def caveats
   	s = <<-EOS.undent
+  		A formula to build the Kicad EDA software suite.
+  	
   		This formula straight-up does not work on OSX systems before Lion. This is not necessarily
   		because they couldn't work, it's because the required c++11 library isn't supported by
   		Apple's clang compiler for targets running older OSX versions.
@@ -56,3 +73,20 @@ class Kicad < Formula
   end
 
 end
+
+__END__
+diff --git a/CMakeModules/download_boost.cmake b/CMakeModules/download_boost.cmake
+index 4a00403..ea3162d 100644
+--- a/CMakeModules/download_boost.cmake
++++ b/CMakeModules/download_boost.cmake
+@@ -143,8 +143,8 @@ if( APPLE )
+     # I set this to being compatible with wxWidgets
+     # wxWidgets still using libstdc++ (gcc), meanwhile OSX
+     # has switched to libc++ (llvm) by default
+-    set( BOOST_CXXFLAGS  "cxxflags=-mmacosx-version-min=10.5  -fno-common" )
+-    set( BOOST_LINKFLAGS "linkflags=-mmacosx-version-min=10.5 -fno-common" )
++    set( BOOST_CXXFLAGS  "cxxflags=-mmacosx-version-min=10.7  -fno-common -stdlib=libc++ -std=c++11" )
++    set( BOOST_LINKFLAGS "linkflags=-mmacosx-version-min=10.7 -fno-common -stdlib=libc++ -std=c++11" )
+     set( BOOST_TOOLSET   "toolset=darwin" )
+ 
+     if( CMAKE_CXX_COMPILER_ID MATCHES "Clang" )
