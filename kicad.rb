ad997765cfb9dab2a0a1b97bc2135ec71f6525d0
David Olmstead <david.lorne.olmstead@gmail.com>
2014-11-06 13:09:14 -0500

    Fixed the pointer comparison error.
    This commit fixes the pointer comparison compiler error. The formula is
    sill not working, however it is now able to compile fully. The next
    step is to fix the install step.

--- kicad.rb
+++ kicad.rb
@@ -9,18 +9,27 @@ class Kicad < Formula
   head "https://code.launchpad.net/~kicad-product-committers/kicad/product", :branch => "lp:kicad", :using => :bzr
 
   depends_on "cmake" => :build
-  depends_on "bzr" => :build
+  #depends_on "bazaar" => :build
   depends_on :macos => [:lion, :build]
   depends_on "wxmac"
   depends_on "zlib"
   depends_on "freeglut"
   depends_on "glew"
   depends_on "doxygen" => :recommended #For doxygen documentation support. See kicad build docs.
-  depends_on "wxpython" => :optional #Will need to develop this more.
+  #depends_on "wxpython" => :optional #Will need to develop this more.
   
-  # This is a patch to fix the internal boost building stupidity. It forces the use of the
+  #option github-plugin See build-config.txt Requires OpenSSL
+  
+  ###########
+  # Patches #
+  ###########
+  # 1. This is a patch to fix the internal boost building stupidity. It forces the use of the
   # c++11 libraries when compiling boost and sets the minimum OSX version to 10.7 so that
-  # goddamned clang and llvm don't have a fit.
+  # clang and llvm don't have a fit.
+  #
+  # 2&3&4. This patch fixes a compiler error in which the compiler thinks that a boolean pointer
+  # is being compared to an integer. This is not the case, and some syntax changes fixed it.
+  
   patch :DATA
   
   # In a crazy world, could implement wxPython support option that adds a bunch of cmake args. See CMakeCache.txt
@@ -30,13 +39,11 @@ class Kicad < Formula
     #Set some environment variables so that we use the correct C libraries when dealing with wxWidgets. I
     #just deprecated this by using the args array to pass these as arguments to cmake rather than setting
     #environment variables.
-    ENV["CC"] = "clang"
+    #ENV["CC"] = "clang"
     # ENV["CXX"] = "clang++"
     # ENV["CXXFLAGS"] = "-stdlib=libc++ -std=c++11 -Wno-c++11-narrowing"
     ENV["OBJCXXFLAGS"] = "-stdlib=libc++ -std=c++11 -Wno-c++11-narrowing"
     ENV["LDFLAGS"] = "-stdlib=libc++ -Wno-c++11-narrowing"
-    
-    # Newest problem: The system's own boost may not be being compiled with libc++11. Fixed by patch?
     
 	# Standard CMAKE args for reference. Remove before release.
 	#      "-DCMAKE_FIND_FRAMEWORK=LAST",
@@ -44,20 +51,20 @@ class Kicad < Formula
 	#      "-DCMAKE_BUILD_TYPE=NONE",
 	#      "-DCMAKE_VERBOSE_MAKEFILE=ON",
 	#      "-Wno-dev"
-
-
-    # The OSX_DEPLOYMENT_TARGET flag is necessary, as clang needs the c++11 library specified,
-    # but won't work unless we also tell it to only build for OSX versions after 10.7.
     
     args = *std_cmake_args
-    args << "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7"
+    args << "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7" # Necessary for clang to use c++11.
+    args << "-DCMAKE_C_COMPILER=clang"
     args << "-DCMAKE_CXX_COMPILER=clang++"
     args << "-DCMAKE_CXX_FLAGS='-stdlib=libc++ -std=c++11 -Wno-c++11-narrowing'"
+    args << "-DKICAD_SCRIPTING=OFF"
+    args << "-DKICAD_SCRIPTING_MODULES=OFF"
+    args << "-DKICAD_SCRIPTING_WXPYTHON=OFF"
     
     ohai "Args array.", *args # Temporary.
     ohai "Standard cmake args.", *std_cmake_args # Temporary.
     
-    system "cmake", ".", *args # Removed: *std_cmake_args, "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.7"
+    system "cmake", ".", *args
 
     system "make", "install"
   end
@@ -85,3 +92,44 @@ index 4a00403..ea3162d 100644
  
      if( CMAKE_CXX_COMPILER_ID MATCHES "Clang" )
 
+diff --git a/eeschema/lib_pin.cpp b/eeschema/lib_pin.cpp
+index f4d997a..8e1a84f 100644
+--- a/eeschema/lib_pin.cpp
++++ b/eeschema/lib_pin.cpp
+@@ -837,7 +837,7 @@ void LIB_PIN::drawGraphic( EDA_DRAW_PANEL*  aPanel,
+     LIB_PART*      Entry = GetParent();
+     bool           DrawPinText = true;
+ 
+-    if( ( aData != NULL ) && ( (bool*) aData == false ) )
++    if( ( aData != NULL ) && ( !aData ) )
+         DrawPinText = false;
+ 
+     /* Calculate pin orient taking in account the component orientation. */
+
+diff --git a/eeschema/onleftclick.cpp b/eeschema/onleftclick.cpp
+index 8a5a6ad..eef8172 100644
+--- a/eeschema/onleftclick.cpp
++++ b/eeschema/onleftclick.cpp
+@@ -147,7 +147,7 @@ void SCH_EDIT_FRAME::OnLeftClick( wxDC* aDC, const wxPoint& aPosition )
+     case ID_JUNCTION_BUTT:
+         if( ( item == NULL ) || ( item->GetFlags() == 0 ) )
+         {
+-            if( false == GetScreen()->GetItem( gridPosition, 0, SCH_JUNCTION_T ) )
++            if( !( GetScreen()->GetItem( gridPosition, 0, SCH_JUNCTION_T ) ) )
+             {
+                 SCH_JUNCTION* junction = AddJunction( aDC, gridPosition, true );
+                 SetRepeatItem( junction );
+
+diff --git a/eeschema/onleftclick.cpp b/eeschema/onleftclick.cpp
+index eef8172..8a67b7a 100644
+--- a/eeschema/onleftclick.cpp
++++ b/eeschema/onleftclick.cpp
+@@ -130,7 +130,7 @@ void SCH_EDIT_FRAME::OnLeftClick( wxDC* aDC, const wxPoint& aPosition )
+     case ID_NOCONN_BUTT:
+         if( ( item == NULL ) || ( item->GetFlags() == 0 ) )
+         {
+-            if( false == GetScreen()->GetItem( gridPosition, 0, SCH_NO_CONNECT_T ) )
++            if( !( GetScreen()->GetItem( gridPosition, 0, SCH_NO_CONNECT_T ) ) )
+             {
+                 SCH_NO_CONNECT*  no_connect = AddNoConnect( aDC, gridPosition );
+                 SetRepeatItem( no_connect );
